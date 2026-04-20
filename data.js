05370a19d55e4b6a2facef249df0c8152ab01891
skoop-n8n-continue <admin1+n8n-continue@skoop.digital>
2026-04-20 23:35:38 +0000

    update

---
+++ data.js
@@ -0,0 +1,29 @@
+const productData = [
+    {
+        category: "Premium Flower",
+        products: [
+            { name: "Blue Dream", type: "Sativa", thc: "22%", price: "$45", image: "https://loremflickr.com/400/400/cannabis,flower?random=1" },
+            { name: "Girl Scout Cookies", type: "Hybrid", thc: "24%", price: "$50", image: "https://loremflickr.com/400/400/cannabis,flower?random=2" },
+            { name: "OG Kush", type: "Indica", thc: "21%", price: "$48", image: "https://loremflickr.com/400/400/cannabis,flower?random=3" },
+            { name: "Sour Diesel", type: "Sativa", thc: "23%", price: "$45", image: "https://loremflickr.com/400/400/cannabis,flower?random=4" }
+        ]
+    },
+    {
+        category: "Pre-Roll Tiers",
+        products: [
+            { name: "Wedding Cake", type: "Hybrid", thc: "20%", price: "$12", image: "https://loremflickr.com/400/400/preroll?random=5" },
+            { name: "Jack Herer", type: "Sativa", thc: "19%", price: "$10", image: "https://loremflickr.com/400/400/preroll?random=6" },
+            { name: "Northern Lights", type: "Indica", thc: "18%", price: "$10", image: "https://loremflickr.com/400/400/preroll?random=7" },
+            { name: "Gelato", type: "Hybrid", thc: "22%", price: "$15", image: "https://loremflickr.com/400/400/preroll?random=8" }
+        ]
+    },
+    {
+        category: "Artisan Edibles",
+        products: [
+            { name: "Gummy Bears", type: "100mg", thc: "10mg/ea", price: "$25", image: "https://loremflickr.com/400/400/gummy?random=9" },
+            { name: "Dark Chocolate", type: "200mg", thc: "20mg/ea", price: "$35", image: "https://loremflickr.com/400/400/chocolate?random=10" },
+            { name: "Peach Rings", type: "100mg", thc: "10mg/ea", price: "$22", image: "https://loremflickr.com/400/400/candy?random=11" },
+            { name: "Fruit Chews", type: "150mg", thc: "15mg/ea", price: "$28", image: "https://loremflickr.com/400/400/fruit?random=12" }
+        ]
+    }
+];
